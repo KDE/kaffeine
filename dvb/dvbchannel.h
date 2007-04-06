ab5c58147e6c16042b82b683c42acc91afe480f9
Christoph Pfister <christophpfister@gmail.com>
2007-04-06 18:39:40 +0000

    add some dvb transponder stuff
    svn path=/branches/work/kaffeine4/kaffeine/; revision=651159

--- dvb/dvbchannel.h
+++ dvb/dvbchannel.h
@@ -23,6 +23,163 @@
 
 #include <config.h>
 
+#include <QString>
+
+class DvbTransponder
+{
+public:
+	enum TransmissionType
+	{
+		DvbC,
+		DvbS,
+		DvbT,
+		Atsc
+	};
+
+	enum FecRate
+	{
+		FecInvalid,
+		Fec1_2,
+		Fec2_3,
+		Fec3_4,
+		Fec4_5,
+		Fec5_6,
+		Fec6_7,
+		Fec7_8,
+		Fec8_9,
+		FecAuto
+	};
+
+	DvbTransponder(TransmissionType transmissionType_,
+		const QString &source_) : transmissionType(transmissionType_),
+		source(source_) { }
+	virtual ~DvbTransponder() { }
+
+	TransmissionType getTransmissionType() const
+	{
+		return transmissionType;
+	}
+
+	QString getSource() const
+	{
+		return source;
+	}
+
+private:
+	TransmissionType transmissionType;
+	QString source;
+};
+
+class DvbCTransponder : public DvbTransponder
+{
+public:
+	enum ModulationType
+	{
+		Qam64,
+		Qam256
+	};
+
+	DvbCTransponder(const QString &source_, uint frequency_,
+		ModulationType modulationType_, uint symbolRate_,
+		FecRate fecRate_) : DvbTransponder(DvbC, source_),
+		frequency(frequency_), modulationType(modulationType_),
+		symbolRate(symbolRate_), fecRate(fecRate_) { }
+	~DvbCTransponder() { }
+
+	/*
+	 * frequency (Hz)
+	 */
+	uint getFrequency() const
+	{
+		return frequency;
+	}
+
+	/*
+	 * modulation type
+	 */
+	ModulationType getModulationType() const
+	{
+		return modulationType;
+	}
+
+	/*
+	 * symbol rate (symbols per second)
+	 */
+	uint getSymbolRate() const
+	{
+		return symbolRate;
+	}
+
+	/*
+	 * FEC rate
+	 */
+	FecRate getFecRate() const
+	{
+		return fecRate;
+	}
+
+private:
+	uint frequency;
+	ModulationType modulationType;
+	uint symbolRate;
+	FecRate fecRate;
+};
+
+class DvbSTransponder : public DvbTransponder
+{
+public:
+	enum Polarization
+	{
+		Horizontal,
+		Vertical
+	};
+
+	DvbSTransponder(const QString &source_, Polarization polarization_,
+		uint frequency_, uint symbolRate_, FecRate fecRate_) :
+		DvbTransponder(DvbS, source_), polarization(polarization_),
+		frequency(frequency_), symbolRate(symbolRate_),
+		fecRate(fecRate_) { }
+	~DvbSTransponder() { }
+
+	/*
+	 * polarization
+	 */
+	Polarization getPolarization() const
+	{
+		return polarization;
+	}
+
+	/*
+	 * frequency (kHz)
+	 */
+	uint getFrequency() const
+	{
+		return frequency;
+	}
+
+	/*
+	 * symbol rate (symbols per second)
+	 */
+	uint getSymbolRate() const
+	{
+		return symbolRate;
+	}
+
+	/*
+	 * FEC rate
+	 */
+	FecRate getFecRate() const
+	{
+		return fecRate;
+	}
+
+private:
+	Polarization polarization;
+	uint frequency;
+	uint symbolRate;
+	FecRate fecRate;
+};
+
 class DvbChannel
 {
 public:
