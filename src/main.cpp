eb9a2b8b0a72b0b80ec34f1a61d105effc445922
Christoph Pfister <christophpfister@gmail.com>
2009-04-24 21:58:09 +0000

    update kaboutdata
    svn path=/trunk/extragear/multimedia/kaffeine/; revision=958878

--- src/main.cpp
+++ src/main.cpp
@@ -61,11 +61,12 @@ int KaffeineApplication::newInstance()
 int main(int argc, char *argv[])
 {
 	KAboutData aboutData("kaffeine", 0, ki18n("Kaffeine"), "0.9.0-pre-alpha",
-		ki18n("A media player for KDE with digital tv support"),
-		KAboutData::License_GPL_V2, ki18n("(C) 2007 The Kaffeine Authors"),
+		ki18n("A media player for KDE with digital TV support"),
+		KAboutData::License_GPL_V2, ki18n("(C) 2007-2009 The Kaffeine Authors"),
 		KLocalizedString(), "http://kaffeine.kde.org");
 
-	// FIXME add authors
+	aboutData.addAuthor(ki18n("Christoph Pfister"), ki18n("Maintainer"),
+		"christophpfister@gmail.com");
 
 	KCmdLineArgs::init(argc, argv, &aboutData);
 	KCmdLineArgs::addCmdLineOptions(Kaffeine::cmdLineOptions());
