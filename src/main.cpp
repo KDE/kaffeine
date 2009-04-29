ca1430cd099e9c1429d8cb0f749e4a1ba53d6382
Andrew Coles <andrew.i.coles@googlemail.com>
2009-04-29 23:08:15 +0000

    SVN_SILENT
    Minor proof-reading - full-stop at end of sentence.
    
    
    svn path=/trunk/extragear/multimedia/kaffeine/; revision=961334

--- src/main.cpp
+++ src/main.cpp
@@ -61,7 +61,7 @@ int KaffeineApplication::newInstance()
 int main(int argc, char *argv[])
 {
 	KAboutData aboutData("kaffeine", 0, ki18n("Kaffeine"), "1.0-svn-" __DATE__,
-		ki18n("A media player for KDE with digital TV support"),
+		ki18n("A media player for KDE with digital TV support."),
 		KAboutData::License_GPL_V2, ki18n("(C) 2007-2009 The Kaffeine Authors"),
 		KLocalizedString(), "http://kaffeine.kde.org");
 
