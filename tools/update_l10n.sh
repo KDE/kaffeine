3945d4b3288f451ab559ce500ab2fece32f37642
Mauro Carvalho Chehab <mchehab@osg.samsung.com>
2016-05-30 16:57:31 -0300

    update_l10n.sh: update to point to the KF5 translations
    The Kaffeine translations metadata were updated. Update the
    script accordingly.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- tools/update_l10n.sh
+++ tools/update_l10n.sh
@@ -18,14 +18,14 @@ endif(NOT GETTEXT_MSGMERGE_EXECUTABLE)
 
 EOF
 
-wget http://websvn.kde.org/*checkout*/trunk/l10n-kde4/subdirs
+wget https://websvn.kde.org/*checkout*/trunk/l10n-kf5/subdirs
 SUBDIRS=$(cat subdirs | grep -vx "x-test")
 rm subdirs
 
 for SUBDIR in $SUBDIRS ; do
 	mkdir $SUBDIR
 	cd $SUBDIR
-	wget http://websvn.kde.org/*checkout*/trunk/l10n-kde4/$SUBDIR/messages/extragear-multimedia/kaffeine.po || true
+	wget https://websvn.kde.org/*checkout*/trunk/l10n-kf5/$SUBDIR/messages/extragear-multimedia/kaffeine.po || true
 	cd ..
 
 	if test -e $SUBDIR/kaffeine.po ; then
